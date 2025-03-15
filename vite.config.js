319101d8cf1ba9cb05353436b7aaaf5fb5596b9f
GaneshN523 <ganeshnalawade523@gmail.com>
2025-03-15 13:32:45 +0530

    resumes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Update the base property to match your repo name.
 export default defineConfig({
-  base: '/resumeviewer/',
+  base: '/resumes/',
   plugins: [react()],
 })
