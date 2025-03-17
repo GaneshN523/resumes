2925dd27665955295e1777c7bf734da9cc8d98d5
GaneshN523 <ganeshnalawade523@gmail.com>
2025-03-17 11:24:37 +0530

    pdf support

--- src/components/ResumeViewer.jsx
+++ src/components/ResumeViewer.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 
 const ResumeViewer = ({ pdf }) => {
   return (
-    <object data={pdf} type="application/pdf" width="100%" height="100%">
-      <p>
-        Your browser does not support PDFs. Please download the PDF to view it: 
-        <a href={pdf}>Download PDF</a>.
-      </p>
-    </object>
+    <div style={{ width: '100%', height: '100%' }}>
+      <embed src={pdf} type="application/pdf" width="100%" height="100%" />
+    </div>
   );
 };
 
