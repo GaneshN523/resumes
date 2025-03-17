561d57351a752eab2e4e9a74d089594313abadd6
GaneshN523 <ganeshnalawade523@gmail.com>
2025-03-17 11:31:06 +0530

    download issue

--- src/components/ResumeViewer.jsx
+++ src/components/ResumeViewer.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const ResumeViewer = ({ pdf }) => {
   return (
     <div style={{ width: '100%', height: '100%' }}>
-      <embed src={pdf} type="application/pdf" width="100%" height="100%" />
+      <iframe
+        src={pdf}
+        style={{ width: '100%', height: '100%', border: 'none' }}
+        title="Resume Viewer"
+      />
     </div>
   );
 };
